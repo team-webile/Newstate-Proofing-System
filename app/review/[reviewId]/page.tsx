fa8f0ec13939b9bece71b764c811d2c9eafa44d5
rshidashrf <zainali24193@gmail.com>
2025-09-30 18:16:44 +0500

    Final update

--- app/review/[reviewId]/page.tsx
+++ app/review/[reviewId]/page.tsx
@@ -56,6 +56,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useToast } from "@/hooks/use-toast";
+import Lightbox from "@/components/Lightbox";
 
 interface ProjectAnnotation {
   id: string;
